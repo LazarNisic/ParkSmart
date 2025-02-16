e370f10cf2585ae986de8ab600d4360303d2aaab
Nisic, Lazar <Lazar.Nisic@ncratleos.com>
2025-02-16 21:36:01 +0100

    Init commit

---
+++ src/main/java/com/lazarnisic/ParkSmart/ParkSmartApplication.java
@@ -0,0 +1,13 @@
+package com.lazarnisic.ParkSmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkSmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkSmartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lazarnisic/ParkSmart/ParkSmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lazarnisic.ParkSmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkSmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
