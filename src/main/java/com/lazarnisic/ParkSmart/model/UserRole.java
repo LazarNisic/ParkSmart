b1f9e576054e432348e02a42cbc946128ace901b
Nisic, Lazar <Lazar.Nisic@ncratleos.com>
2025-02-27 15:50:54 +0100

    Add User and UserRole models

---
+++ src/main/java/com/lazarnisic/ParkSmart/model/UserRole.java
@@ -0,0 +1,22 @@
+package com.lazarnisic.ParkSmart.model;
+
+import com.lazarnisic.ParkSmart.enums.Role;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "user_role", schema = "public")
+public class UserRole {
+    //create table
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @ManyToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
