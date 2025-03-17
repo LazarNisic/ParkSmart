d1ef64d2867d6c21815dd776a2da28a30aa095db
Lazar Nisić <lazarenetel@gmail.com>
2025-03-17 09:46:03 +0100

    Create ParkingSpot service, repositoies, controller; Add getAvailableParkingSpots method

---
+++ src/main/java/com/lazarnisic/ParkSmart/controller/ParkingSpotController.java
@@ -0,0 +1,2 @@
+package com.lazarnisic.ParkSmart.controller;public class ParkingSpotController {
+}
---
+++ src/main/java/com/lazarnisic/ParkSmart/repository/ParkingSpotRepository.java
@@ -0,0 +1,2 @@
+package com.lazarnisic.ParkSmart.repository;public interface ParkingSpotRepository {
+}
